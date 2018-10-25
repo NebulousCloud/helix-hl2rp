5531effda37494d0bd8e7a4cb8e3d397c8bde275
Alexander Grist-Hucker <gristhavoc@gmail.com>
2018-10-25 19:48:34 +0100

    Fix Voices help menu.
    Closes #2

--- schema/cl_hooks.lua
+++ schema/cl_hooks.lua
@@ -171,21 +171,80 @@ function Schema:BuildBusinessMenu(panel)
 	return bHasItems
 end
 
-function Schema:BuildHelpMenu(tabs)
-	tabs["voices"] = function(node)
-		local body = {}
+function Schema:PopulateHelpMenu(tabs)
+	tabs["voices"] = function(container)
+		local classes = {}
 
-		for k, v in SortedPairs(Schema.voices.stored) do
-			body[#body + 1] = "<h2>"..string.upper(k).."</h2>"
-
-			for k2, v2 in SortedPairs(v) do
-				body[#body + 1] = "<div><strong>"..string.upper(k2).."</strong> <em>"..v2.text.."</em></div>"
+		for k, v in pairs(Schema.voices.classes) do
+			if (v.condition(LocalPlayer())) then
+				classes[#classes + 1] = k
 			end
-
-			body[#body + 1] = "<br /><br />"
 		end
 
-		return table.concat(body)
+		if (#classes < 1) then
+			local info = container:Add("DLabel")
+			info:SetFont("ixSmallFont")
+			info:SetText("You do not have access to any voice lines!")
+			info:SetContentAlignment(5)
+			info:SetTextColor(color_white)
+			info:SetExpensiveShadow(1, color_black)
+			info:Dock(TOP)
+			info:DockMargin(0, 0, 0, 8)
+			info:SizeToContents()
+			info:SetTall(info:GetTall() + 16)
+
+			info.Paint = function(_, width, height)
+				surface.SetDrawColor(ColorAlpha(derma.GetColor("Error", info), 160))
+				surface.DrawRect(0, 0, width, height)
+			end
+
+			return
+		end
+
+		table.sort(classes, function(a, b)
+			return a < b
+		end)
+
+		for _, class in ipairs(classes) do
+			local category = container:Add("Panel")
+			category:Dock(TOP)
+			category:DockMargin(0, 0, 0, 8)
+			category:DockPadding(8, 8, 8, 8)
+			category.Paint = function(_, width, height)
+				surface.SetDrawColor(Color(0, 0, 0, 66))
+				surface.DrawRect(0, 0, width, height)
+			end
+
+			local categoryLabel = category:Add("DLabel")
+			categoryLabel:SetFont("ixMediumLightFont")
+			categoryLabel:SetText(class:upper())
+			categoryLabel:Dock(FILL)
+			categoryLabel:SetTextColor(color_white)
+			categoryLabel:SetExpensiveShadow(1, color_black)
+			categoryLabel:SizeToContents()
+			category:SizeToChildren(true, true)
+
+			for command, info in SortedPairs(self.voices.stored[class]) do
+				local title = container:Add("DLabel")
+				title:SetFont("ixMediumLightFont")
+				title:SetText(command:upper())
+				title:Dock(TOP)
+				title:SetTextColor(ix.config.Get("color"))
+				title:SetExpensiveShadow(1, color_black)
+				title:SizeToContents()
+
+				local description = container:Add("DLabel")
+				description:SetFont("ixSmallFont")
+				description:SetText(info.text)
+				description:Dock(TOP)
+				description:SetTextColor(color_white)
+				description:SetExpensiveShadow(1, color_black)
+				description:SetWrap(true)
+				description:SetAutoStretchVertical(true)
+				description:SizeToContents()
+				description:DockMargin(0, 0, 0, 8)
+			end
+		end
 	end
 end
 
