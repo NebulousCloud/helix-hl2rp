589922c28506958c46dab86ed4d2cf675d126bcf
Alexander Grist-Hucker <gristhavoc@gmail.com>
2019-01-12 20:03:46 +0000

    Add Resistance and Medic uniform.
    Closes #9.

---
+++ schema/items/outfit/sh_resistance_uniform.lua
@@ -0,0 +1,11 @@
+
+ITEM.name = "Resistance Uniform"
+ITEM.description = "A resistance uniform with a symbol on the sleeve."
+ITEM.category = "Clothing"
+ITEM.flag = "v"
+
+ITEM.replacements = {
+	{"group01", "group03"},
+	{"group02", "group03"},
+	{"group03m", "group03"},
+}
---
+++ schema/items/outfit/sh_rmedic_uniform.lua
@@ -0,0 +1,11 @@
+
+ITEM.name = "Medic Uniform"
+ITEM.description = "A resistance uniform with medical insignia on the sleeve."
+ITEM.category = "Clothing"
+ITEM.flag = "v"
+
+ITEM.replacements = {
+	{"group01", "group03m"},
+	{"group02", "group03m"},
+	{"group03", "group03m"},
+}
