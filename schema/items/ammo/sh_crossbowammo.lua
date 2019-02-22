69af3a8cfa4db22337c0145f3a550439f60dc1df
Wolffe <Wolffebusiness@outlook.com>
2019-02-21 23:24:24 -0500

    Fix invalid ammotype for Crossbow Bolts

--- schema/items/ammo/sh_crossbowammo.lua
+++ schema/items/ammo/sh_crossbowammo.lua
@@ -1,5 +1,5 @@
 ITEM.name = "Crossbow Bolts"
 ITEM.model = "models/Items/BoxBuckshot.mdl"
-ITEM.ammo = "XBowRounds" -- type of the ammo
+ITEM.ammo = "XBowBolt" -- type of the ammo
 ITEM.ammoAmount = 5 -- amount of the ammo
 ITEM.description = "A Bundle of %s Crossbow Bolts"
