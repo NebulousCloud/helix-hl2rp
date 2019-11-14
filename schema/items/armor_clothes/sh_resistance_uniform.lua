29a62fe124ca5d2a1a4bd33dd679b934890da2d1
Alexander Grist-Hucker <gristhavoc@gmail.com>
2019-11-14 23:15:19 +0000

    Add armour to Resistance Uniform

--- schema/items/armor_clothes/sh_resistance_uniform.lua
+++ schema/items/armor_clothes/sh_resistance_uniform.lua
@@ -3,6 +3,7 @@ ITEM.name = "Resistance Uniform"
 ITEM.description = "A resistance uniform with a symbol on the sleeve."
 ITEM.category = "Clothing"
 ITEM.flag = "v"
+ITEM.maxArmor = 50
 
 ITEM.replacements = {
 	{"group01", "group03"},
---
+++ schema/items/base/sh_armor_clothes.lua
@@ -0,0 +1,43 @@
+
+ITEM.base = "base_outfit"
+ITEM.name = "Armored Clothes"
+ITEM.description = "A suitcase full of clothes."
+ITEM.model = Model("models/props_c17/suitcase_passenger_physics.mdl")
+ITEM.category = "Armored Clothing"
+ITEM.width = 2
+ITEM.height = 2
+ITEM.maxArmor = 0
+
+if (CLIENT) then
+	function ITEM:PopulateTooltip(tooltip)
+		local panel = tooltip:AddRowAfter("name", "armor")
+		panel:SetBackgroundColor(derma.GetColor("Warning", tooltip))
+		panel:SetText("Armor: " .. (self:GetData("equip") and LocalPlayer():Armor() or self:GetData("armor", self.maxArmor)))
+		panel:SizeToContents()
+	end
+end
+
+function ITEM:OnEquipped()
+	self.player:SetArmor(self:GetData("armor", self.maxArmor))
+end
+
+function ITEM:OnUnequipped()
+	self:SetData("armor", math.Clamp(self.player:Armor(), 0, self.maxArmor))
+	self.player:SetArmor(0)
+end
+
+function ITEM:Repair(amount)
+	self:SetData("armor", math.Clamp(self:GetData("armor") + amount, 0, self.maxArmor))
+end
+
+function ITEM:OnLoadout()
+	if (self:GetData("equip")) then
+		self.player:SetArmor(self:GetData("armor", self.maxArmor))
+	end
+end
+
+function ITEM:OnSave()
+	if (self:GetData("equip")) then
+		self:SetData("armor", math.Clamp(self.player:Armor(), 0, self.maxArmor))
+	end
+end
