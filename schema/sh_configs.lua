951a7af7fe28578c6e93df7020f6ced976abc99c
Alexander Grist-Hucker <gristhavoc@gmail.com>
2020-03-08 06:57:38 +0000

    Change default maxAttributes value

--- schema/sh_configs.lua
+++ schema/sh_configs.lua
@@ -5,6 +5,7 @@ ix.currency.plural = "tokens"
 
 ix.config.SetDefault("scoreboardRecognition", true)
 ix.config.SetDefault("music", "music/hl2_song19.mp3")
+ix.config.SetDefault("maxAttributes", 60)
 
 ix.config.Add("rationTokens", 20, "The amount of tokens that a person will get from a ration", nil, {
 	data = {min = 0, max = 1000},
