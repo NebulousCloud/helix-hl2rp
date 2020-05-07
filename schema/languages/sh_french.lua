5d0164796c4a4db1f2b2a3177564c0c7855ccf42
EnderHero <TheRealEnderHero@users.noreply.github.com>
2020-05-07 19:39:31 +0200

    Add french translations (#36)

---
+++ schema/languages/sh_french.lua
@@ -0,0 +1,52 @@
+
+NAME = "Français"
+
+LANGUAGE = {
+	schemaName = "Half-Life 2 Roleplay",
+	tying = "Cette personne est en train d'être attachée...",
+	unTying = "Cette personne est en train d'être détachée...",
+	isTied = "Attaché",
+	fTiedUp = "Vous avez été attaché.",
+	fBeingTied = "Vous êtes attaché.",
+	tiedUp = "Ils ont été attachés.",
+	beingTied = "Ils sont en train d'être attachés.",
+	beingUntied = "Ils sont en train d'être détachés.",
+	needRequestDevice = "Vous avez besoin d'un dispositif de requête pour faire ça.",
+	vendingNeedMoney = "Vous avez besoin de %s tokens pour l'acheter.",
+	vendingPurchased = "Vous avez acheté une boisson pour %s.",
+	vendingRestocked = "Vous avez renouvelé le stock de boissons.",
+	cantViewData = "Vous ne pouvez pas consulter les données de cette personne.",
+	cid = "CID",
+	citizenid = "ID Citoyen",
+	lastEdit = "Dernière révision par",
+	lastEditDate = "Dernière révision le",
+	objectivesTitle = "Objectifs en cours",
+	searchingCorpse = "Fouille du cadavre...",
+	radioNotOn = "Votre radio n'est pas allumée !",
+	radioRequired = "Vous avez besoin d'une radio pour faire ça.",
+	radioAlreadyOn = "Vous avez déjà une radio qui est allumée !",
+	economy = "Économie",
+
+	-- combine display messages
+	cViewData = "Téléchargement des données du profil citoyen...",
+	cViewDataExpired = "ERREUR ! Échec du téléchargement des données révisées du profil citoyen...",
+	cViewDataUpdate = "Mise en ligne des données révisées du profil citoyen...",
+	cViewDataFiller = "Mise en cache des données révisées du profil citoyen...",
+	cViewObjectives = "Téléchargement du manifeste des objectifs de la Milice...",
+	cViewObjectivesUpdate = "Mise en ligne du manifeste révisé des objectifs de la Milice...",
+	cViewObjectivesFiller = "Récupération du manifeste des objectifs de la Milice (RÉVISION %d)...",
+	cCivilJudgement = "Préparation des protocoles d'administration des jugements civils...",
+	cRequest = "Téléchargement du dossier de demande...",
+	cCitizenLoaded = "Reconstruction du manifeste citoyen...",
+	cCombineLoaded = "Mise à jour des coordonnées du biosignal..",
+	cLostBiosignal = "Téléchargement du biosignal perdu...",
+	cLostBiosignalLocation = "AVERTISSEMENT ! Perdu biognal pour unité de protection à %s...",
+	cTrauma = "AVERTISSEMENT ! %s traumatisme détecté...",
+	cDroppingVitals = "AVERTISSEMENT ! FONCTIONS VITALES EN DIMINUTION, ASSISTANCE MÉDICALE REQUISE",
+	cStaminaLost = "ATTENTION : Effort excessif de l'utilisateur, administration d'un stimulant...",
+	cStaminaGained = "STIMULANT ADMINISTRÉ",
+	cLosingContact = "Téléchargement des informations du contact radio perdu...",
+	cLostContact = "AVERTISSEMENT ! Perdu contact radio pour unité de protection à un emplacement inconnu...",
+
+	voices = "Voix"
+}
