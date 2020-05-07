d654522360c20e97e2a8bf17b0f8a52180536a71
JohnyReaper <40298237+JohnyReaper@users.noreply.github.com>
2020-05-07 19:37:46 +0200

    Fix typing beeps for non-combine (#39)

--- schema/sv_hooks.lua
+++ schema/sv_hooks.lua
@@ -269,8 +269,10 @@ function Schema:PlayerMessageSend(speaker, chatType, text, anonymous, receivers,
 					if (info.global) then
 						netstream.Start(nil, "PlaySound", info.sound)
 					else
-						speaker.bTypingBeep = nil
-						ix.util.EmitQueuedSounds(speaker, {info.sound, "NPC_MetroPolice.Radio.Off"}, nil, nil, volume)
+						if (speaker:IsCombine()) then
+							speaker.bTypingBeep = nil
+							ix.util.EmitQueuedSounds(speaker, {info.sound, "NPC_MetroPolice.Radio.Off"}, nil, nil, volume)
+						end
 					end
 				end
 
